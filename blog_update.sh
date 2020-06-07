42d1a31cbcc0823e69b6188fca1b30b1cded0a0b
royce.li <private.royce@gmail.com>
2020-06-07 15:37:05 +0800

    shell modify

--- blog_update.sh
+++ blog_update.sh
@@ -7,15 +7,15 @@ git pull
 echo "============================  docker image build... ============================ "
 docker build --build-arg CACHEBUST=$(date +%s) --rm -t royce-hugo-docker .
 
-echo "============================ docker none image remove... ============================ "
-# 不加-f, 则基础的build产生的image不会被清除
-docker images|grep none|awk '{print $3 }'|xargs docker rmi
-
 echo "============================ docker stop ============================ "
 docker stop royce-blog
 
 echo "============================ docker rm ============================ "
 docker rm royce-blog
+
+echo "============================ docker none image remove... ============================ "
+# 不加-f, 则基础的build产生的image不会被清除
+docker images|grep none|awk '{print $3 }'|xargs docker rmi
 
 echo "============================ docker run royce-blog...============================ "
 docker run --name royce-blog -d -p 1880:1880  royce-hugo-docker
