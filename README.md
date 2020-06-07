c04f131cd44d9fe9c6b8abb747a54b9136a36e97
royce.li <private.royce@gmail.com>
2020-06-07 13:03:56 +0800

    shell脚本一键更新

--- README.md
+++ README.md
@@ -14,5 +14,5 @@ docker build --build-arg CACHEBUST=$(date +%s) --rm -t royce-hugo-docker .
 ### run container
 
 ```
-docker run --name {container_name} -d -p {host_port}:80  hugo-docker
+docker run --name royce-blog -d -p 1880:1880  royce-hugo-docker
 ```
--- blog_update.sh
+++ blog_update.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 echo "Start Blog Update..."
 
-cd $0
-
-echo "cd $0, pull..."
-
 git pull
 
-docker build --rm -t $0 .
+docker build --build-arg CACHEBUST=$(date +%s) --rm -t royce-hugo-docker .
 
-docker run --name {container_name} -d -p {host_port}:$1  hugo-docker
+# docker run --name royce-blog -d -p 1880:1880  royce-hugo-docker
+
+docker restart 4d65bd8b8abd
