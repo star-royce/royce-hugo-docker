655a4c0a9c0d6c77c932e1968d661c54e2281673
royce.li <private.royce@gmail.com>
2020-06-07 14:59:21 +0800

    shell restart docker

--- blog_update.sh
+++ blog_update.sh
@@ -14,4 +14,4 @@ docker build --build-arg CACHEBUST=$(date +%s) --rm -t royce-hugo-docker .
 
 # docker run --name royce-blog -d -p 1880:1880  royce-hugo-docker
 echo "============================ docker container restart...============================ "
-docker restart 4d65bd8b8abd
+docker restart 59efd44114ff
