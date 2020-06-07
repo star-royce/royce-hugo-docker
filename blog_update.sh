5b97634c31a096d926d0e316571eec2248a9981e
royce.li <private.royce@gmail.com>
2020-06-07 13:12:21 +0800

    shell脚本echo

--- blog_update.sh
+++ blog_update.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 echo "Start Blog Update..."
 
+echo "Git Project Update..."
 git pull
 
+echo "docker image build..."
 docker build --build-arg CACHEBUST=$(date +%s) --rm -t royce-hugo-docker .
 
 # docker run --name royce-blog -d -p 1880:1880  royce-hugo-docker
-
+echo "docker container restart..."
 docker restart 4d65bd8b8abd
