28be543798bb7c7aa2c12db1d3f7aade8459cef5
royce.li <private.royce@gmail.com>
2020-06-07 14:13:41 +0800

    shell脚本 modify

--- blog_update.sh
+++ blog_update.sh
@@ -10,7 +10,7 @@ docker build --build-arg CACHEBUST=$(date +%s) --rm -t royce-hugo-docker .
 
 echo "============================ docker none image remove... ============================ "
 # 不加-f, 则基础的build产生的image不会被清除
-ocker images|grep none|awk '{print $3 }'|xargs docker rmi
+docker images|grep none|awk '{print $3 }'|xargs docker rmi
 
 # docker run --name royce-blog -d -p 1880:1880  royce-hugo-docker
 echo "============================ docker container restart...============================ "
