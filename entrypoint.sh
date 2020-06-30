673c350b93c4781f113b1343bb07bf674f1c3776
royce <royce.li@klook.com>
2020-06-30 17:07:24 +0800

    nginx restart

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,7 +12,7 @@ echo "============================ docker run royce-blog...=====================
 docker run --name royce-blog -d -p 1880:1880 royce-blog
 
 echo "============================ docker public download...============================ "
-docker cp royce-blog:/tmp/public/* /opt/web/royce-blog/public/
+docker cp royce-blog:/tmp/public /opt/web/royce-blog/
 
 echo "============================ docker stop ============================ "
 docker stop royce-blog
@@ -20,6 +20,9 @@ docker stop royce-blog
 echo "============================ docker rm ============================ "
 docker rm royce-blog
 
-#echo "============================ docker none image remove... ============================ "
+echo "============================ docker none image remove... ============================ "
 # 不加-f, 则基础的build产生的image不会被清除
 docker images|grep none|awk '{print $3 }'|xargs docker rmi
+
+echo "============================ nginx docker restart... ============================ "
+docker-compose restart nginx
