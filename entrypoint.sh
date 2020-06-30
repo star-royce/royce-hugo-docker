60774b8c06eccfb22650d3a84944940f01c669ca
royce <royce.li@klook.com>
2020-06-30 16:00:09 +0800

    shell 脚本优化

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-cd blog
-
 echo "============================  Start Blog Update... ============================ "
 
 echo "============================ Git Project Update... ============================ "
 git pull
 
 echo "============================  docker image build... ============================ "
-docker build --build-arg CACHEBUST=$(date +%s) --rm -t royce-blog .
+docker build --build-arg CACHEBUST="$0" --rm -t royce-blog .
 
 echo "============================ docker run royce-blog...============================ "
 docker run --name royce-blog -d -p 1880:1880 royce-blog
@@ -22,6 +20,6 @@ docker stop royce-blog
 echo "============================ docker rm ============================ "
 docker rm royce-blog
 
-echo "============================ docker none image remove... ============================ "
+#echo "============================ docker none image remove... ============================ "
 # 不加-f, 则基础的build产生的image不会被清除
-docker images|grep none|awk '{print $3 }'|xargs docker rmi
+#docker images|grep none|awk '{print $3 }'|xargs docker rmi
