4ea2dc45aea4f8acdb4ecc722f20b4a53d3c525e
royce <royce.li@klook.com>
2020-06-30 15:36:50 +0800

    main docker

--- Dockerfile
+++ Dockerfile
@@ -2,30 +2,4 @@ FROM alpine:latest
 
 LABEL maintainer=private.royce@gmail.com
 
-# 指定使用阿里云国内源
-RUN sed -i 's/dl-cdn.alpinelinux.org/mirrors.aliyun.com/g' /etc/apk/repositories \
-    && apk add --update git libc6-compat libstdc++
-
-# 下太慢，改为本地
-ADD ./soft/hugo_extended_0.72.0_Linux-64bit.tar.gz /usr/local/bin/
-RUN rm -rf ./soft/hugo_extended_0.72.0_Linux-64bit.tar.gz
-
-# 让tmp目录在后续步骤中可用
-WORKDIR /tmp
-
-ENV GIT_REPOSITORY=https://github.com/star-royce/royce-hugo.git
-ENV GIT_REPOSITORY_NAME=royce-hugo
-
-# 这一步开始，不使用缓存, CACHEBUST由build指令传入当前时间
-ARG CACHEBUST=1
-
-# 拉取最新代码
-RUN git clone ${GIT_REPOSITORY} \
-    && cd /tmp/${GIT_REPOSITORY_NAME} \
-    && hugo \
-    && mv public /tmp \
-    && cd /tmp \
-    && rm -rf ${GIT_REPOSITORY_NAME}
-
-
 ENTRYPOINT ["entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,6 @@
 
 cd blog
 
-
-
 echo "============================  Start Blog Update... ============================ "
 
 echo "============================ Git Project Update... ============================ "
@@ -11,6 +9,12 @@ git pull
 
 echo "============================  docker image build... ============================ "
 docker build --build-arg CACHEBUST=$(date +%s) --rm -t royce-blog .
+
+echo "============================ docker run royce-blog...============================ "
+docker run --name royce-blog -d -p 1880:1880 royce-blog
+
+echo "============================ docker public download...============================ "
+docker cp royce-blog:/tmp/public /opt/web/royce-blog/
 
 echo "============================ docker stop ============================ "
 docker stop royce-blog
@@ -21,9 +25,3 @@ docker rm royce-blog
 echo "============================ docker none image remove... ============================ "
 # 不加-f, 则基础的build产生的image不会被清除
 docker images|grep none|awk '{print $3 }'|xargs docker rmi
-
-echo "============================ docker run royce-blog...============================ "
-docker run --name royce-blog -d -p 1880:1880 royce-blog
-
-echo "============================ docker public download...============================ "
-docker cp royce-blog:/tmp/public /opt/web/royce-blog/
